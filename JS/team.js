65c952fba491ded9ee86afe411ca5f0ce914ac83
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-01-23 16:02:51 +0100

    feat: add autowriting Title

--- JS/team.js
+++ JS/team.js
@@ -1,37 +1,42 @@
 const team = [
   {
-    name: "Wayne Barnett",
-    role: "Founder & CEO",
+    name: "Jonathan Lombardo",
+    role: "The MemeMan",
     image: "wayne-barnett-founder-ceo.jpg",
   },
   {
-    name: "Angela Caroll",
-    role: "Chief Editor",
+    name: "Vincenzo Mercadante",
+    role: "The Machine",
     image: "angela-caroll-chief-editor.jpg",
   },
   {
-    name: "Walter Gordon",
-    role: "Office Manager",
+    name: "Nicola Gallo",
+    role: "The SuperPartes",
     image: "walter-gordon-office-manager.jpg",
   },
   {
-    name: "Angela Lopez",
-    role: "Social Media Manager",
+    name: "Michele Ianni",
+    role: "The BigPrinter",
     image: "angela-lopez-social-media-manager.jpg",
   },
   {
-    name: "Scott Estrada",
-    role: "Developer",
+    name: "Simone Pucci",
+    role: "The Pari Nine Man",
     image: "scott-estrada-developer.jpg",
   },
   {
-    name: "Barbara Ramos",
-    role: "Graphic Designer",
+    name: "Mattia Teoria",
+    role: "The CalmKeeper",
     image: "barbara-ramos-graphic-designer.jpg",
   },
 ];
 
 const rowEl = document.querySelector("#cards-row");
+const titleEl = document.querySelector("#title");
+const subtitleEl = document.querySelector("#subtitle");
+
+writingDelay("OUR TEAM", titleEl, 80);
+writingDelay("Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id vitae perferendis molestiae natus. Obcaecati, incidunt?", subtitleEl, 0);
 
 for (let member of team) {
   rowEl.innerHTML += `
